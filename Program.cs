b6d9a825f41f3c00f60aa2e824be8a86516cd16a
antjamespadgett <padgettanthonyj@gmail.com>
2021-10-30 15:10:24 -0500

    did a little work

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace cSharpMasterClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring several variables at once
+
+            int num3, num4, num5;
+
+            //Declaring variable
+            int num1;
+            //assigning variable
+            num1 = 13;
+            //Declaring and assigning variable
+            int num2 = 31;
+            num2 = 127;
+            int sum = num1 + num2;
+
+            
+
+            Console.WriteLine($"num1: {num1} + num2: {num2} is {sum}");
+
+            double d1 = 3.23;
+            double d2 = 4.24;
+
+            double dDiv = d1 / d2;
+            Console.WriteLine($"d1/d2 is {dDiv}");
+
+
+            float f1 = 3.23f;
+            float f2 = 4.24f;
+
+            float fDiv = f1 / f2;
+            Console.WriteLine($"f1/f2 is {fDiv}");
+
+
+           
+            Console.ReadLine();
+        }
+    }
+}
